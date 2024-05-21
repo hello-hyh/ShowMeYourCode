abb7fa79546d1152fe3ae511686039718e493a94
hello-hyh <15055667366@qq.com>
2024-05-21 18:16:06 +0800

    ✨ feat:  del some obsolete code

---
+++ src/components/navbar.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <div class="h-[50px] bg-black w-full">
+    <div class="">
+      <img src="" />
+    </div>
+  </div>
+</template>
+<style lang="less" scoped></style>
---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Navbar from '../../components/navbar.vue'
+</script>
+<template>
+  <Navbar ></Navbar>
+  <div class="text-lime-200">1231231</div>
+</template>
