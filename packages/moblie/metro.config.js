6651940ad934461c1af780428372cff257697bea
hello-hyh <15055667366@qq.com>
2024-06-03 18:46:22 +0800

    ✨ feat: add react-native project

---
+++ packages/moblie/metro.config.js
@@ -0,0 +1,40 @@
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
+
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const path = require('path');
+
+const projectRoot = __dirname;
+
+const workspaceRoot = path.resolve(projectRoot, '../..');
+console.log(path.resolve(projectRoot, 'node_modules'), path.resolve(workspaceRoot, 'node_modules'));
+
+
+const extraNodeModules = {
+  'backend': path.resolve(path.join(__dirname, '../../backend')),
+  'frontend': path.resolve(path.join(__dirname, '../../frontend'))
+};
+const watchFolders = [
+  projectRoot,
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+const nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+module.exports = mergeConfig(getDefaultConfig(__dirname), {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: true,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    extraNodeModules,
+    nodeModulesPaths
+  },
+  watchFolders
+}) 
