c6e2411f8bdaf6c330ef3ef2b31b57da8fb29ea6
hello-hyh <15055667366@qq.com>
2024-06-20 17:12:18 +0800

    upd: frontend project add repl.

--- packages/frontend/src/components/navbar.vue
+++ packages/frontend/src/components/navbar.vue
@@ -15,12 +15,10 @@ onMounted(() => {})
     />
     <el-dropdown>
       <div class="w-12 flex items-center justify-center">
-        <el-avatar :size="28" :src="userStore.userInfo.avatar_url" />
+        <el-avatar :size="28" :src="userStore.userInfo?.avatar_url" />
       </div>
       <template #dropdown>
-        <el-dropdown-menu>
-          <el-dropdown-item>Action 1</el-dropdown-item>
-        </el-dropdown-menu>
+        <el-dropdown-menu> </el-dropdown-menu>
       </template>
     </el-dropdown>
   </div>
--- packages/frontend/src/views/home/HomeView.vue
+++ packages/frontend/src/views/home/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Navbar from '../../components/navbar.vue'
+import { Repl } from '@vue/repl'
+import Monaco from '@vue/repl/monaco-editor'
 </script>
 <template>
-  <Navbar ></Navbar>
-  <div class="bg-white px-2">
-
+  <Navbar></Navbar>
+  <div class="bg-white px-2 h-[calc(100vh-50px)]">
+    <Repl :editor="Monaco" />
   </div>
 </template>
