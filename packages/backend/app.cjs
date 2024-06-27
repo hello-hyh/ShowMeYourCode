d0c2fe212d7625cd8fc935e66256713768ee5b98
hello-hyh <15055667366@qq.com>
2024-06-27 14:14:45 +0800

    upd: test ci.

--- packages/backend/app.cjs
+++ packages/backend/app.cjs
@@ -1,8 +1,2 @@
-module.exports = {
-  apps: [{
-    name: 'showmeyourcode-backend',
-    script: './src/index.ts',
-    interpreter: './node_modules/.bin/tsx',
-    instances: 1,
-  }]
-};
+var cmd = require('node-cmd');
+cmd.run('pnpm run serve');
